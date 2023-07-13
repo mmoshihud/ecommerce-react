254cb97550f2620ed378cf1a49772393793d0884
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-07-14 03:15:43 +0600

    react router and tailwind setup

---
+++ src/layouts/App.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to React</h1>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1>Hi, I am from React Home</h1>
+      <h1 className="font-bold text-3xl underline">Hello world! sa</h1>
+    </>
+  );
+};
+
+export default Home;

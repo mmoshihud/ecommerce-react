505ee32e6bfe5470861c4d4fe22c2b4b4ec3217d
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-07-14 02:38:19 +0600

    login using axios and get token

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Login = () => {
+  const [csrf, setCsrf] = useState("");
+
+  const apiClient = axios.create({
+    baseURL: "http://localhost:8000",
+    headers: {
+      "X-CSRFToken": csrf,
+    },
+    withCredentials: true, // Ensure cookies are sent
+  });
+
+  useEffect(() => {
+    apiClient
+      .get("/get-csrf-token")
+      .then(function (response) {
+        setCsrf(response.data.csrfToken);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const handleLoginForm = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    document.cookie = `csrftoken=${csrf}; path=/`;
+    apiClient
+      .post("/user/login", { email: email, password: password })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <>
-      <form action="" method="post">
+      <form onSubmit={handleLoginForm}>
         <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="" />
+        <input type="text" name="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" />
+        <button type="submit">Log in</button>
       </form>
     </>
   );

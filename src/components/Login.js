892e969c8d18d89fa5edd9fcb303d4e339182c46
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-07-13 16:43:50 +0600

    project setup

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <>
+      <form action="" method="post">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="" />
+      </form>
+    </>
+  );
+};
+
+export default Login;
